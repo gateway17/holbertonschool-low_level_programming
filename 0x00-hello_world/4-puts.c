0b3dacd7cdd27940f45eca61cb3f3f4a0473e06a
gateway17 <gateway17ssh@gmail.com>
2020-09-18 22:22:36 +0000

    Done.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+  * main - print message
+  *
+  *Return: print requested message (success)
+  */
+char main(void)
+{
+	char str01[54] = "\"Programming is like building a multilingual puzzle";
+	puts(str01);
+	return (0);
+}
