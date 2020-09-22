25de14baca4bb39682730cf4d1f5f89c21b786d0
gateway17 <gateway17ssh@gmail.com>
2020-09-22 17:39:53 +0000

    Done.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Alphabet
+ * print the alphabet.
+ * Return: return 0 if code exit successfully
+ */
+int main(void)
+{
+	char ab, xz;
+
+	ab = 'a';
+	xz = 'z';
+	while (ab <= xz; ab++)
+	{
+		putchar(ab);
+	}
+	return (0);
+}
