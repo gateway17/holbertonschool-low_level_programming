285f45da6c9e197c464241d5017c0450e3b5b0ef
gateway17 <gateway17ssh@gmail.com>
2020-09-22 21:39:13 +0000

    Done.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Decimal numbers
+ * Print all decimals numbers
+ * Return: 0 if everything goes as planned
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num = num + 1)
+	{
+	printf("%d", num);
+	}
+	putchar ('\n');
+	return (0);
+}
