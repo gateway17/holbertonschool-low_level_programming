d350741de32d79825be0ba8a34f52c72e4f6b9b7
gateway17 <gateway17ssh@gmail.com>
2020-09-22 04:22:14 +0000

    Done.

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	a = n % 10;
 	if (a > 5)
-	printf("Last digit of %d is %d\n", n, a);
+	printf("Last digit of %d is %d and is greater than 5", n, a);
 	else if (a == 0)
 	printf("Last digit of %d is %d and is 0\n", n, a);
 	else if ((a < 6) && (a != 0))
