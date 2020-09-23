6eba2e2e5e255dd09dcd4cf0f4b35db806c23bf3
gateway17 <gateway17ssh@gmail.com>
2020-09-23 22:53:10 +0000

    Done.

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,13 +1,19 @@
 #include"holberton.h"
-
+/**
+ *main - Print "holberton"
+ *Print "holberton" bringing the function "_putchar" fron "holberton.h"
+ *Return: return 0 when program finish.
+ */
 int main(void)
 {
-	char c[] ="Holberton";
+	char c[] = "Holberton";
+
 	int counter;
+
 	for (counter = 0; c[counter] != 00; counter++)
 	{
-		_putchar(c[counter]);
+		_putchar (c[counter]);
 	}
-_putchar('\n');
-return(0);
+_putchar ('\n');
+return (0);
 }
