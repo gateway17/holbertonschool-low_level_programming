2446c8d65c7ce780cc409fa34f5706a9f520ae11
gateway17 <gateway17ssh@gmail.com>
2020-09-25 22:52:37 +0000

    Done.

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 /**
  * _isdigit - digits or not.
- *@c: valiable for the digit.
- *Return: 1 if it is digit
+ * @c: valiable for the digit.
+ * Return: 1 if it is digit
  */
+
 int _isdigit(int c)
 {
 	if (c >= 0 && c <= 10)
