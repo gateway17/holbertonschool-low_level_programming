cc0c3c3fe2065b3ce79614981a969f0a1c4a92e5
gateway17 <gateway17ssh@gmail.com>
2020-09-25 18:40:02 +0000

    Done.

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,21 @@
 #include"holberton.h"
+/**
+ *_abs - value of a integer.
+ *Computes the absolute value of an integer.
+ *@a: integer that you enter.
+ *Return: 0 always.
+ */
 
 int _abs(int a)
 {
 
-	if (a >= 48 && a <= 57)
+	if (a > 0)
 	{
-		_putchar(a);
+		return (a);
 	}
-return(0);
+	else
+	{
+		return (a * -1);
+	}
+return (0);
 }
