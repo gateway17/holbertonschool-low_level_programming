a15ad5945abaacd080ba7e1d00ad65e8c9f14b0a
FELIPE <felipe@NNRX2000>
2020-09-27 18:30:59 -0500

    Done

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Fizz Buzz.
+ * print Fizz if the number is muntiple of 3, Buzz if multiple of 5.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num1;
+	for (num1 = 1; num1 <= 100; num1++)
+	{
+		if (num1 % 3 == 0 && num1 % 5 == 0)
+		{
+			printf ("FizzBuzz ");
+			continue;
+		}
+		if (num1 % 3 == 0)
+		{
+			printf ("Fizz ");
+			continue;
+		}
+		else if ((num1 % 5) == 0)
+		{
+			printf ("Buzz ");
+			continue;
+		}
+		printf ("%d ", num1);
+	}
+	putchar ('\n');
+return(0);
+}
