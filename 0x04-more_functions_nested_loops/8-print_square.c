96c85c8c0c31ae526db3c3bc548c395f26258eba
gateway17 <gateway17ssh@gmail.com>
2020-09-27 01:44:33 +0000

    Done.

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ *print_square - loop for printing a square.
+ *@size: the size of the squere.
+ */
+void print_square(int size)
+{
+	int counter1, counter2;
+
+	for (counter1 = 0; counter1 < size; counter1++)
+	{
+		for (counter2 = 0; counter2 < size; counter2++)
+		{
+			_putchar(35);
+		}
+	_putchar ('\n');
+	}
+	if (size == 0)
+	{
+		_putchar ('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-	print_diagonal(10);
+	print_square(10);
 return(0);
 }
