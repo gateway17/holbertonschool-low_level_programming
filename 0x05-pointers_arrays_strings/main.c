572232aad2d5b577dc1db3f8650d09c2ef676477
gateway17 <gateway17ssh@gmail.com>
2020-10-01 00:34:52 +0000

    Done.

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
  * main - check the code for Holberton School students.
@@ -10,7 +9,7 @@ int main(void)
 {
     char *str;
 
-    	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    	str = " awmdio wikxm 1332 okdao!*?)=(";
 	/*printf("La posicion del primer caracter es: %p\n", &str[]); */
 	print_rev(str);
     return (0);
