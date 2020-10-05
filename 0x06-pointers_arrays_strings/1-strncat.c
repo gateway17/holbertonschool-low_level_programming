a90a9874f7cecbe84ef7410684868baf5dd84a62
gateway17 <gateway17ssh@gmail.com>
2020-10-05 22:49:56 +0000

    Done.

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -22,7 +22,6 @@ char *_strncat(char *dest, char *src, int n)
 		for (var2 = 0; var2 <= n; var2++)
 		{
 			var3 = dest[ctr + var2] = src[var2];
-			_putchar (var3);
 		}
 		_putchar('\n');
 return (dest);
