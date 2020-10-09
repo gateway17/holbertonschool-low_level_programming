7da70e514a4fa68d563188b6bd09aa0a59249fca
gateway17 <gateway17ssh@gmail.com>
2020-10-09 04:19:59 +0000

    Done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,34 +1,35 @@
-#include "holberton.h"
+#include <stdio.h>
 /**
- *jack_bauer - print the Jack Bauer clock.
+ *jack_bauer -  prints Jack bauer's clock
  */
 void jack_bauer(void)
 {
-	char min2, min1, hr1, hr2;
-
+	int hr1, hr2, min1, min2;
 
 
 	for (hr1 = 48; hr1 < 51; hr1++)
 	{
+		for (hr2 = 48; hr2 < 58 ; hr2++)
+		{
 
-	for (hr2 = 48; hr2 < 52; hr2++)
-	{
+			for (min1 = 48; min1 < 54; min1++)
+			{
 
-	for (min1 = 48; min1 < 54; min1++)
-	{
+				for (min2 = 48; min2 < 58; min2++)
+				{
 
-
-	for (min2 = 48; min2 < 58; min2++)
-	{
-		_putchar(hr1);
-		_putchar(hr2);
-		_putchar(':');
-		_putchar(min1);
-		_putchar(min2);
-		_putchar('\n');
+					putchar (hr1);
+				if (hr1 == 50 && hr2 == 52)
+				{
+					return;
+				}
+				putchar (hr2);
+				putchar (':');
+				putchar (min1);
+				putchar (min2);
+				putchar ('\n');
+				}
+			}
+		}
 	}
-	}
-	}
-	}
-
 }
