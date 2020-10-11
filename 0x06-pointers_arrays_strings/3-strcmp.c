92d02fae5e34ed0d9b7f6047d092054da9c7c536
gateway17 <gateway17ssh@gmail.com>
2020-10-11 20:09:49 +0000

    Done.

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,18 +1,36 @@
 #include "holberton.h"
-
-
+/**
+ *_strcmp - compares two strings.
+ *@s1:First string.
+ *@s2:Second string.
+ *Return:if s1 is longer (15), if s1 is shorter (-15),both equal in length (0)
+ */
 int _strcmp(char *s1, char *s2)
 {
-	for (ctr = 0; 
 
+	int ctr1 = 0;
+	int ctr2 = 0;
 
+	while (s1[ctr1] != '\0')
+	{
+		ctr1++;
+	}
 
-		if (*s + ctr != *s2)
-		{
+	while (s2[ctr2] != '\0')
+	{
+		ctr2++;
+	}
 
-
-		}
-
-
-
+	if (ctr1 > ctr2)
+	{
+		return (15);
+	}
+	else if (ctr1 < ctr2)
+	{
+		return (-15);
+	}
+	else if (ctr1 == ctr2)
+	{
+		return (0);
+	}
 }
