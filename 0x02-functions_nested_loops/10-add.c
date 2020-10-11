915add3223980ac6c37edafaa165e5f7ecbb377c
gateway17 <gateway17ssh@gmail.com>
2020-10-11 01:58:59 +0000

    Done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ *add - Adds two integers and retuns the result.
+ *@f: First integer.
+ *@s: Second integer to add.
+ *Return:returns the result.
+*/
+int add(int f, int s)
+{
+	return (f + s);
+}
