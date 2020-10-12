ee2eb6cf0964edc9fbfc28e00a816865da1cc279
gateway17 <gateway17ssh@gmail.com>
2020-10-12 19:20:56 +0000

    Done.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-
+/**
+ *main - Print the name of the program.
+ *@argc:Number of parameter for the main program, (in this case not used).
+ *@argv:Parameters for main program.
+ *Return:Always 0.
+ */
 int main(int __attribute__((__unused__)) argc, char **argv)
 {
-	printf("%s\n",*argv);
-	return(0);
+	printf("%s\n", *argv);
+	return (0);
 
 }
