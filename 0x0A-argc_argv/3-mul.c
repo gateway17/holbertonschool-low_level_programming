a1a2e752ad9580136f554eebe79a30292189c275
gateway17 <gateway17ssh@gmail.com>
2020-10-12 22:25:30 +0000

    Done.

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - make a multiplication with the parameters after the main program.
+ *@argc: NO USED,
+ *@argv: integers to multiply.
+ *Return: "0" if there are more than 2 parameters (after de main program)
+ *	"1" if not.
+ */
+int main(int __attribute__((__unused__)) argc, char **argv)
+{
+
+	if (argc > 2)
+	{
+		printf("%d\n ", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+	else
+	{
+		printf("Error.\n");
+		return (1);
+	}
+}
