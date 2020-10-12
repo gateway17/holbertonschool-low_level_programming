4b5a274edb10ab12a97c0fceaa31391ba4e3275e
gateway17 <gateway17ssh@gmail.com>
2020-10-12 21:29:10 +0000

    Done.

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
-
-
+/**
+ *main - Print each parameters.
+ *@argc:counts the mount of parameters.
+ *@argv:Parameters
+ *Return:Always 0.
+ */
 int main(int argc, char *argv[])
 {
-	if (ctr <= argc
-		printf("%s\n", argv[ctr])
+	int ctr;
 
-
+	for (ctr = 0; ctr < argc; ctr++)
+	{
+	printf("%s\n", argv[ctr]);
+	}
+	return (0);
 
 }
