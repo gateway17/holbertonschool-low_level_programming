b5ec27c462bc113797948491d0dd69214e49e8d7
gateway17 <gateway17ssh@gmail.com>
2020-10-17 00:56:28 +0000

    Done.

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int factorial(int n)
+{
+	int result;
+	int
+	int  
+/*	printf("El valor de n ahora es: %d\n",result);
+*/
+	if (n >= 1)
+	{
+	result = factorial (n * (ctr - 1));
+	}
+	ctr--;
+	printf("El valor de n ahora es: %d\n",result);
+
+		return(result);
+}
+
+int main(void)
+{
+    int r;
+
+    r = factorial(5);
+    printf("%d\n", r);
+	return(0);
+}
