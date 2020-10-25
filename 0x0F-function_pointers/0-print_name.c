3731f32c2e4831dd1f0d9cb83d658e1a0b2f72df
gateway17 <gateway17ssh@gmail.com>
2020-10-25 16:55:47 +0000

    Done.

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * print_name - calls other function.
+ * @name: name of the person
+ * @f: pionter to the name.
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
