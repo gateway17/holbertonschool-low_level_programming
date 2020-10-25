416a999680162c7ab9790ae7fbbf13ed14740e11
gateway17 <gateway17ssh@gmail.com>
2020-10-25 19:23:13 +0000

    Done.

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "function_pointers.h"
 
 /**
  * array_iterator - iterants each position of a array
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,9 @@
 #ifndef PRINT_NAME
 #define PRINT_NAME
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+
+
 
 #endif
