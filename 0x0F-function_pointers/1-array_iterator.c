4b900c5c3d1e5cd8c271342d0f414389d200a327
gateway17 <gateway17ssh@gmail.com>
2020-10-25 19:21:21 +0000

    Done.

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterants each position of a array
+ * and calls another function.
+ *@array: The array.
+ *@size: The mount of positions we are going to print.
+ *@action: The function we are calling.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int counter;
+
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+
+	for (counter = 0; size > 0; counter++, --size)
+	{
+		action(array[counter]);
+
+	}
+}
