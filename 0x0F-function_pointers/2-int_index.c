acee4cafb3199d85781d72e8c52d25945e840bb6
gateway17 <gateway17ssh@gmail.com>
2020-11-05 02:11:21 +0000

    Done.

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - call another function to compare two numbers.
+ * @array: Array of numbers.
+ * @size: Size of the array.
+ * @cmp: function we are going to call.
+ * Return: Position in the array where numbers match.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+
+	if (cmp == NULL || array == NULL ||  size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+
+	if (cmp(array[i]) > 0)
+	{
+		return (i);
+	}
+
+	}
+
+}
