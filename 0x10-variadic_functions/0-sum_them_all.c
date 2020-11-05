b92261acb697332b0df12f2fb7240fcda99ccca9
gateway17 <gateway17ssh@gmail.com>
2020-11-05 23:21:11 +0000

    Done.

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,14 +6,14 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	if (n == 0)
+	unsigned int salida;
 
-		return (0);
+	unsigned int i;
 
 	va_list(list);
 	va_start(list, n);
-	unsigned int salida;
-	unsigned int i;
+	if (n == 0)
+		return (0);
 
 	for (i = 0; i < n; i++)
 	{
