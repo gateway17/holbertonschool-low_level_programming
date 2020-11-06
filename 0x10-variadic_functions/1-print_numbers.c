64a48f6d7a14643aaf991f6e6ef7133d1929699a
gateway17 <gateway17ssh@gmail.com>
2020-11-06 16:49:36 +0000

    Done.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -6,16 +6,23 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    unsigned int ctr;
+	unsigned int ctr;
+
 	va_list(list);
 	va_start(list, n);
-	
+
 
 	for (ctr = 0; ctr < n - 1 ; ctr++)
 	{
+	if (separator == NULL)
+	{
+		printf("%d", va_arg(list, unsigned int));
+	}
+	else
+	{
 	printf("%d%s", va_arg(list, unsigned int), separator);
 	}
-
+	}
 	printf("%d\n", va_arg(list, unsigned int));
-
+	va_end(list);
 }
