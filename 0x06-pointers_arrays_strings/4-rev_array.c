1b1b1b34b3f33ae326f328249b3b3016ffb9a4a6
gateway17 <gateway17ssh@gmail.com>
2020-11-29 21:02:15 +0000

    Done.

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,56 +1,21 @@
 #include <stdio.h>
 #include "holberton.h"
-
+/**
+ *reverse_array - Reverse de values of an array.
+ *@a: Array.
+ *@n: number of elements of the array.
+ */
 void reverse_array(int *a, int n)
 {
-	int ctr2 = 0;
-	int ctr1 = 0;
+	n--;
+	int ctr2 = 0, ctr1 = n, b = 0;
 
-	while (a[ctr1] != '\0')
+	for (; ctr1 >= ctr2; ctr1--)
 	{
-		printf("%d\n", a[ctr1]);
-		ctr1++;
-	}
-
-/*	while (ctr2 <= n)
-	{
+	ctr2 = n - ctr1;
+	b = a[ctr2];
 	a[ctr2] = a[ctr1];
-	
-	ctr2++;
-	ctr1--;
-	} */
-
-}
-
-
-void print_array(int *a, int n)
-{
-    int i;
-
-    i = 0;
-    while (i < n)
-    {
-        if (i != 0)
-        {
-            printf(", ");
-        }
-        printf("%d", a[i]);
-        i++;
-    }
-    printf("\n");
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int a[] = {12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
-
-    print_array(a, sizeof(a) / sizeof(int));
-    reverse_array(a, 12);
-    print_array(a, sizeof(a) / sizeof(int));
-    return (0);
+	a[ctr1] = b;
 	}
+
+}
