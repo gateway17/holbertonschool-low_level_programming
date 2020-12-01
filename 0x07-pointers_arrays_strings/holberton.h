67114c979ad37d52e558b594fc21b6297be6c08c
gateway17 <gateway17ssh@gmail.com>
2020-12-01 01:18:33 +0000

    Done.

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,6 +1,9 @@
+#include <stdio.h>
+
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
