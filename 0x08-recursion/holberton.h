8eee2ca09bcfad432410ffb31928c6579385f6d7
gateway17 <gateway17ssh@gmail.com>
2020-12-02 17:58:42 +0000

    Done.

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -11,6 +11,8 @@ int factorial(int n);
 
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-int is_palindrome(char *s)
+int is_palindrome(char *s);
+int function(int ctr, char *s, int size);
+int _strlen(char *s);
 
 #endif
