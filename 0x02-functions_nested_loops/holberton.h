57e58e26bb57eb9822b0c95050e042b3653c494b
gateway17 <gateway17ssh@gmail.com>
2020-12-04 03:10:21 +0000

    Done.

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -10,3 +10,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int f, int s);
 void print_to_98(int n);
+void print_times_table(int n);
+int middleDigit(int n);
+void check(int *b, int *e, int *n);
